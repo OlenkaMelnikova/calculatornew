79a72d843f8a1f73307a9c6f784be869afae82c4
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-10-25 22:01:02 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+#include "calculatornew.hpp"
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cin >> a;
+    cin >> b;
+
+  cout << sum(a, b) << endl;
+  cout << sub(a, b) << endl;
+  cout << pow1(a, b) << endl;
+  cout << dev(a, b) << endl;
+  cout << sqrt1(a) << endl;
+  
+}
