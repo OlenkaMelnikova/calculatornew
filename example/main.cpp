950d573b0af9584b2e89011051eb56fa841d58d9
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-10-31 09:08:06 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -6,13 +6,27 @@ using namespace std;
 int main()
 {
     float a, b;
+    
+double sum(float a, float b);
+double sub(float a, float b); 
+double pow1(float a, float b);
+double dev(float a, float b);
+double sqrt1(float a);
+       
+    setlocale(LC_ALL,"rus");
+    
+    cout<< "Введите значение a\n ";
     cin >> a;
+    cout<< "Введите значение b\n ";
     cin >> b;
-
-  cout << sum(a, b) << endl;
-  cout << sub(a, b) << endl;
-  cout << pow1(a, b) << endl;
-  cout << dev(a, b) << endl;
-  cout << sqrt1(a) << endl;
+    
+   cout<<"\nCумма равна: " << sum(a,b);
+   cout<<"\nРазность равна: "<< sub(a,b);
+   cout<<"\nПроизведение равно: "<< multiplication(a,b);
+   cout<<"\nЧастное равно: "<< dev(a,b);
+   cout<<"\nквадратный корень из c равен: "<<sqrt1(a) ;
   
+    
+    
+     return 0;
 }
