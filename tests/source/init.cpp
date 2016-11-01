bec7a7cf41112e920435de5488254bc8090d28dc
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-11-01 16:03:02 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -3,39 +3,47 @@
 
 SCENARIO("calculator sum", "[sum]") {
     float a = 4.0;
-    float b = 17.0;
+    float b = 8.0;
     
     double rv = sum( a, b );
     
-	REQUIRE( rv == 21.0 );
+	REQUIRE( rv == 12.0 );
 }
 SCENARIO("calculator sub", "[sub]") {
     float a = 4.0;
-    float b = 17.0;
+    float b = 8.0;
     
     double rv = sub( a, b );
     
-	REQUIRE( rv == -13.0 );
+	REQUIRE( rv == -4.0 );
 }
 SCENARIO("calculator pow1", "[pow1]") {
     float a = 4.0;
-    float b = 17.0;
+    float b = 8.0;
     
-    double rv = pow1( x, y );
+    double rv = pow1( a, b );
     
-	REQUIRE( rv == 68.0 );
+	REQUIRE( rv == 32.0 );
 }
 SCENARIO("calculator dev", "[dev]") {
     float a = 4.0;
-    float b = 17.0;
+    float b = 8.0;
     
     double rv = dev( a, b );
     
-	REQUIRE( rv == 0.235294 );
+	REQUIRE( rv == 0.5 );
+}
+SCENARIO("calculator involution", "[involution]") {
+    float a = 4.0;
+    float b = 8.0;
+    
+    double rv = involution( a, b );
+    
+	REQUIRE( rv == 65536.0 );
 }
 SCENARIO("calculator sqrt1", "[sqrt1]") {
     float a = 4.0;
-    float b = 17.0;
+    float b = 8.0;
     
     double rv = sqrt1( a, b );
     
