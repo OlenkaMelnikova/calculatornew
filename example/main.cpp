1f56683c29ab2e9c1991c48070bd50f8e96860ff
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-11-01 15:13:03 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -11,6 +11,7 @@ double sum(float a, float b);
 double sub(float a, float b); 
 double pow1(float a, float b);
 double dev(float a, float b);
+double involution(float a, float b);
 double sqrt1(float a);
        
     setlocale(LC_ALL,"rus");
@@ -24,6 +25,7 @@ double sqrt1(float a);
    cout<<"\nРазность равна: "<< sub(a,b);
    cout<<"\nПроизведение равно: "<< multiplication(a,b);
    cout<<"\nЧастное равно: "<< dev(a,b);
+   cout<<"\na в степени c равно: "<< involution(a,b);
    cout<<"\nквадратный корень из c равен: "<<sqrt1(a) ;
   
     
