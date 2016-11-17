6fa43c477f4b44e7dfcf85067e44811768f286a3
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-11-17 20:12:26 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -2,5 +2,5 @@ float sum(float a, float b);
 float sub(float a, float b);
 float pow1(float a, float b);
 float dev(float a, float b);
-float involution(float a, unsigned int b);
+float involution(float a, float b);
 float sqrt1(float a);
