1884880af64e2c31125f3ec708c1d78fc54d995b
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-12-18 18:18:34 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -13,10 +13,9 @@ int main() {
     cout<< "Введите значение b\n ";
     cin >> b;
     cin >> c;
-    
-    cout<<"\nCумма равна: " << sum(a,b);
-    cout<<"\nРазность равна: "<< sub(a,b);
-    cout<<"\nПроизведение равно: "<< pow1(a,b);
+    cout << roundp(sum(a, b)) << endl;	
+    cout << roundp(sub(a, b)) << endl;	
+    cout << roundp(pow1(a, b)) << endl;
     double * res = dev(a, b);
 	if (res!=nullptr)
 		cout << "a / b = " << * res << endl;
@@ -27,9 +26,7 @@ int main() {
 	if (r) cout << "a^0.5=" << *sqrt1(a) << endl;
 	else cout << "a < 0 -> error" << endl;
 	delete r;    
-   
-    cout<<"\na в степени c равно: "<< involution(a,b);
-
-  
+    cout << roundp(involution(a, b)) << endl;
+	
  system("pause");
 }
