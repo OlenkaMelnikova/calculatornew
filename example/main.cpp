1dd9222ca5604f976bce31447ddd52a0b59e2cde
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-12-18 17:55:05 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -4,20 +4,31 @@ using namespace std;
 
 int main() {
 
-        float a;
-        float b;
+        double a;
+        double b;
+        int c;	
     
     cout<< "Введите значение a\n ";
     cin >> a;
     cout<< "Введите значение b\n ";
     cin >> b;
+    cin >> c;
     
     cout<<"\nCумма равна: " << sum(a,b);
     cout<<"\nРазность равна: "<< sub(a,b);
     cout<<"\nПроизведение равно: "<< pow1(a,b);
-    cout<<"\nЧастное равно: "<< dev(a,b);
+    double * res = dev(a, b);
+	if (res!=nullptr)
+		cout << "a / b = " << * res << endl;
+	else
+		cout << "a/0 -> error" << endl;
+	delete res;
+    double *r = sqrt1(a);
+	if (r) cout << "a^0.5=" << *sqrt1(a) << endl;
+	else cout << "a < 0 -> error" << endl;
+	delete r;    
+   
     cout<<"\na в степени c равно: "<< involution(a,b);
-    cout<<"\nквадратный корень из c равен: "<<sqrt1(a) ;
 
   
  system("pause");
