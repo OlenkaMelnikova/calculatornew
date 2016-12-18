13dab5b9b58c69c8306a06d60f10e26f7aa4d1b3
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-12-18 18:20:40 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -4,3 +4,5 @@ double pow1(double a, double b);
 double *dev(double a, double b);
 double *sqrt1(double a);
 double involution(double a, double b);
+double absolut(double a);
+double roundp(double a);
