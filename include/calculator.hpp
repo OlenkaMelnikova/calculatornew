8d61a12c570a2663ca2b1f96c9fd8e9295fc3578
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2017-05-19 01:51:06 +0300

    Delete calculator.hpp

--- include/calculator.hpp
+++
@@ -1,8 +0,0 @@
-double sum(double a, double b);
-double sub(double a, double b);
-double pow1(double a, double b);
-double *dev(double a, double b);
-double *sqrt1(double a);
-double involution(double a, double b);
-double absolut(double a);
-double roundp(double a);
