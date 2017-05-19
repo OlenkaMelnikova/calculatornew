1f0182873ea7f18528bb69d93e493fa21421fa8f
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2017-05-19 10:17:48 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -4,87 +4,121 @@
 
 SCENARIO("complex init") 
 {
-	TComplex complex;
+	complex_number complex;
 	REQUIRE(complex.real_() == 0);
-	REQUIRE(complex.imaginary_() == 0);
+	REQUIRE(complex.image_() == 0);
 }
-SCENARIO("param") 
+SCENARIO("complex with params") 
 {
-	complex a(2, 2);
-	REQUIRE(a.im() == 2);
-	REQUIRE(a.re() == 2);
+	complex_number complex(2,3);
+	REQUIRE(complex.real_() == 2);
+	REQUIRE(complex.image_() == 3);
 }
 
 SCENARIO("copy")
 {
-	complex a (4, 4), b(a);	
-	REQUIRE(a.im() ==b.im());
-	REQUIRE(a.re() == b.re());
+	complex_number ob (4, 4);
+	complex_number od(ob);	
+	REQUIRE(od.image_() ==4);
+	REQUIRE(od.real_() == 4);
 	
 }
 
 SCENARIO("+")
 {
-	complex a (4, 4), b(a);
-	complex c(8, 8);
-	b+=a;
-	REQUIRE(b== c);
+	complex_number A (4, 4);
+	complex_number B(A);
+	complex_number C(8, 8);
+	REQUIRE(A+B== c);
 }
 
 SCENARIO("*") 
 {
-	complex b(3, 3), a(1,1), c(0,6);
-	b*=a;
-	REQUIRE(b==c);
+	complex_number B(3, 3);
+ 	complex_number A(1,1);
+	complex_number C(0,6);
+	REQUIRE(A*B==c);
 }
 
 SCENARIO("/") 
 {
-	complex b(3, 3), a(9,9), c(3, 0);
-	a/=b;
-	REQUIRE(a==c);
+	complex_number B(3, 3), A(9,9), C(3, 0);
+	REQUIRE(A/B==c);
+}
+SCENARIO("+=")
+{
+	complex_number A(4, 5);
+	complex_number B(4, 5);
+	complex_number C(8, 10);
+	REQUIRE((A+=B)==C);
 }
 
+SCENARIO("-=")
+{
+	complex_number A(9, 9);
+	complex_number B(4, 5);
+	complex_number C(5, 4);
+	REQUIRE((A-=B)==C);
+}
+
+SCENARIO("*=")
+{
+	complex_number A(4, 5);
+	complex_number B(4, 5);
+	complex_number C(-9, 40);
+	REQUIRE((A*=B)==C);
+}
+
+SCENARIO("/=")
+{
+	complex_number A(4, 5);
+	complex_number B(4, 5);
+	complex_number C(1, 0);
+	REQUIRE((A/=B)==C);
+}
 SCENARIO("==") 
 {
-	complex a(4, 3);
-	complex b (a);
-	REQUIRE(a == b);
+	complex_number A(4, 3);
+	complex_number B (A);
+	bool f;
+	if (A==B) 
+		f=true;
+	REQUIRE(f == true);
 }
 
 SCENARIO("=") {
-	complex a(7, 5);
-	complex f = a;
-	REQUIRE(f == a);
+	complex_number A(7, 5);
+	complex_number F = A;
+	REQUIRE(F == A);
 }
 
 SCENARIO("-") {
-	complex a(5, 4), b(3, 3), f(2, 1);
+	complex_number a(5, 4), b(3, 3), f(2, 1);
 	a-=b;
 	REQUIRE(f == a);
 }
 SCENARIO("add")
 {
-	complex a (4, 4), b(a);
-	complex c(8, 8);
+	complex_number a (4, 4), b(a);
+	complex_number c(8, 8);
 	b=b.add(a);
 	REQUIRE(b== c);
 }
 SCENARIO("mult") 
 {
-	complex b(3, 3), c(9,9);
+	complex_number b(3, 3), c(9,9);
 	b=b.mult(3);
 	REQUIRE(b==c);
 }
 
 SCENARIO("mydiv") 
 {
-	complex b(3, 3), c(1,1);
+	complex_number b(3, 3), c(1,1);
 	b=b.mydiv(3);
 	REQUIRE(b==c);
 }
 SCENARIO("dif") {
-	complex a(9, 9), b(3, 3), f(6, 6);
+	complex_number a(9, 9), b(3, 3), f(6, 6);
 	a=a.dif(b);
 	REQUIRE(f == a);
 }
